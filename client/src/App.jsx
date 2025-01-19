c51fa5090a6d53f3068f509b2f885a7f6bee3d04
Jeyachandran J <125851197+jeyachandranj@users.noreply.github.com>
2025-01-19 14:13:06 +0530

    add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
 import Login from './components/Login';
 import SignUp1 from './components/SignUp1';
 import SignUp2 from './components/SignUp2';
 
 const App = () => {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-200">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup1" element={<SignUp1 />} />
-          <Route path="/signup2" element={<SignUp2 />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <HashRouter>
+            <div className="min-h-screen bg-gray-200">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup1">Sign Up 1</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup2">Sign Up 2</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup1" element={<SignUp1 />} />
+                    <Route path="/signup2" element={<SignUp2 />} />
+                </Routes>
+            </div>
+        </HashRouter>
+    );
 };
 
 export default App;
